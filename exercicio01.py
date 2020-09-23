c018b70cf55f1bcbd6e4966e0c0928d722788aec
arturfabriciohahaedgy <arturfabricio99@gmail.com>
2020-09-23 15:45:38 -0300

    feat: atividade-Ip-01

---
+++ exercicio01.py
@@ -0,0 +1,7 @@
+minhaTupla = (1, 2, "Artur", "Caramelo", 4, 5)
+
+minhaTupla[2] = "Fabricio"
+print(minhaTupla)
+
+#ocorre um erro, pois a tulpa não foi feita para ter o valor alterado sobre outro
+#por isso tem a descrição "tuple object does not support item assignment"
