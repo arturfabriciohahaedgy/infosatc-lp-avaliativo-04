9358ec12127c683a09f5352b14f04f5eeb40a8c5
arturfabriciohahaedgy <arturfabricio99@gmail.com>
2020-09-23 17:32:24 -0300

    fix: atividade-lp-01, agora com input.

--- exercicio01.py
+++ exercicio01.py
@@ -1,7 +1,9 @@
+palavraNormal = input("Insira uma palavra para a inversão: ")
+
 def parametroInvertido(palavra):
     palavraInvertida = palavra[::-1]
     print(palavraInvertida)
 
-parametroInvertido("ATOR")
+parametroInvertido(palavraNormal)
 
 #o comando [::-1] serve pra inverter a palavra
